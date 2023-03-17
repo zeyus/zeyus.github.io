1572a287f1a76c8d6670b3d14042e6563a4bdfad
zeyus <zeyus@zeyus.com>
2023-03-17 02:35:12 +0100

    night.

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
-const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -18,7 +17,7 @@ const config = {
 		  strict: true
 		}),
 		paths: {
-			base: dev ? '' : '/zeyus.com',
+			base: '',
 		},
 		output: {
 			preloadStrategy: 'preload-mjs',
