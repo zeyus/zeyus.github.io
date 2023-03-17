47b152e24a67ad58f219fd3621f427ac42549504
zeyus <zeyus@zeyus.com>
2023-03-17 02:18:07 +0100

    ok.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+    <title>zeyus dot com</title> 
+</svelte:head>
+<nav>
+	<a href="/">home</a>
+</nav>
+<h1>zeyus dot com</h1>
+<Paper color="secondary" variant="outlined">
+    <Title>evolution</Title>
+    <Content>coming soon...</Content>
+  </Paper>
+<script lang="ts">
+    import Paper, { Title, Subtitle, Content } from '@smui/paper';
+</script>
---
+++ svelte.config.js
@@ -0,0 +1,29 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+const dev = process.argv.includes('dev');
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit:  {
+		adapter: adapter({
+		  pages: 'build',
+		  assets: 'build',
+		  fallback: 'index.html',
+		  precompress: false,
+		  strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/zeyus.github.io',
+		},
+		output: {
+			preloadStrategy: 'preload-mjs',
+	  	}
+	}
+};
+
+export default config;
