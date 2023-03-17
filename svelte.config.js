3d20159844a4a8d339da73ce80178b02cb7a419a
zeyus <zeyus@zeyus.com>
2023-03-17 02:24:31 +0100

    yes.

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 		  strict: true
 		}),
 		paths: {
-			base: dev ? '' : '/zeyus.github.io',
+			base: dev ? '' : '/zeyus.com',
 		},
 		output: {
 			preloadStrategy: 'preload-mjs',
