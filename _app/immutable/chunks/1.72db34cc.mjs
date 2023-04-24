10f16ee9f55d4cc53f1b6c3f6801c76fc82d42a7
zeyus <zeyus@zeyus.com>
2023-04-24 11:54:27 +0200

    Updates

---
+++ _app/immutable/chunks/1.72db34cc.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.eae27366.mjs";export{t as component};
--- _app/immutable/chunks/1.78a7edf5.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e64b946c.mjs";export{t as component};
