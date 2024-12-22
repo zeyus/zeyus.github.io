e41305285c6596767f6f8952e09a8b71db5738a1
zeyus <dev@zeyus.com>
2024-12-22 23:32:26 +0100

    ok, my bad

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { type Config } from '@sveltejs/kit';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	extensions: ['.svelte'],
