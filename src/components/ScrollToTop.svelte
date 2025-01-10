4cb061e3c97db81451b1a5fcfc339bac5fe8599b
zeyus <zeyus@zeyus.com>
2025-01-10 11:07:35 +0100

    Added aria label to scroll to top.

--- src/components/ScrollToTop.svelte
+++ src/components/ScrollToTop.svelte
@@ -19,6 +19,7 @@
     <button
         class="fixed bottom-4 right-4 z-50 p-2 bg-primary-600 dark:bg-primary-600 text-white rounded-full shadow-lg"
         on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        aria-label="Scroll to top"
     >
         <ArrowUpOutline class="w-6 h-6" />
     </button>
